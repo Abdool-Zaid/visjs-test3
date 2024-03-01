27819df157437dd91462bc75ad0d104b79033a5e
Abdool-Zaid <aztoorabally7447@gmail.com>
2024-03-01 15:38:45 +0200

    arrows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,30 +20,11 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('visualizationContainer') visualizationContainer!: ElementRef;
 
   showModal: boolean = false;
+  selectedNode: Number = 1;
   network!: Network;
-  nodes: Node[] = [
-    { id: 1, label: 'Node 1' },
-    { id: 2, label: 'Node 2' },
-    { id: 3, label: 'Node 3' },
-    { id: 4, label: 'Node 4' },
-    { id: 5, label: 'Node 5' },
-    { id: 6, label: 'Node 6' },
-    { id: 7, label: 'Node 7' },
-    { id: 8, label: 'Node 8' },
-    { id: 9, label: 'Node 9' },
-  ];
+  nodes: Node[] = [{ id: 1, label: 'Node 1' }];
 
-  edges: Edge[] = [
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-    { from: 2, to: 6 },
-    { from: 2, to: 7 },
-    { from: 2, to: 8 },
-    { from: 2, to: 9 },
-    { from: 9, to: 3 },
-  ];
+  edges: Edge[] = [];
 
   ngAfterViewInit(): void {
     let data = {
@@ -66,12 +47,11 @@ export class AppComponent implements AfterViewInit {
     );
     this.network.setOptions(options);
 
-    // Attach click event listener to the network
     this.network.on('click', (params) => {
-      // Check if a node was clicked
       if (params.nodes.length > 0) {
         const nodeId = params.nodes[0];
-        this.onNodeClick(nodeId);
+        this.selectedNode = nodeId;
+        // this.onNodeClick(nodeId);
       }
     });
     this.network.on('dragEnd', (params) => {
@@ -95,7 +75,7 @@ export class AppComponent implements AfterViewInit {
 
               // Check if the distance is within a threshold
 
-              if (distance < 100) {
+              if (distance < 20) {
                 // Adjust threshold as needed
                 console.log(
                   `Node ${draggedNodeId} is close to node ${node.id}`
@@ -126,29 +106,48 @@ export class AppComponent implements AfterViewInit {
 
     this.network.setData({ nodes: this.nodes, edges: this.edges });
   }
-  // Function to execute when a node is clicked
-  onNodeClick(nodeId: string): void {
+  onNodeClick(
+    nodeId: string,
+    shape: string,
+    label: string,
+    edgeType: string
+  ): void {
     this.showModal = true;
     console.log('Node clicked:', nodeId);
     const selectedNode = Number(nodeId);
 
     const newNodeId = this.nodes.length + 1;
-    this.nodes.push({ id: newNodeId, label: 'Added Node' });
+    this.nodes.push({ id: newNodeId, shape: shape, label: label });
 
-    const newEdge: Edge = { from: selectedNode, to: newNodeId };
-    this.edges.push(newEdge);
+    if (edgeType == 'arrow') {
+      const newEdge: Edge = { from: selectedNode, to: newNodeId, arrows:'to' };
+      this.edges.push(newEdge);
+    }else{
+      const newEdge: Edge = { from: selectedNode, to: newNodeId };
+      this.edges.push(newEdge);
+    }
     this.update_network();
   }
   add_node(event: Event) {
     event.preventDefault();
 
-    const nodeNameInput = document.getElementById('node_name') as HTMLInputElement;
-    const selectedShapeInput = document.querySelector('input[name="shape"]:checked') as HTMLInputElement;
+    const nodeNameInput = document.getElementById(
+      'node_name'
+    ) as HTMLInputElement;
+    const selectedShapeInput = document.querySelector(
+      'input[name="shape"]:checked'
+    ) as HTMLInputElement;
+    const edgeTypeInput = document.querySelector(
+      'input[name="connector"]:checked'
+    ) as HTMLInputElement;
+    const edgeType = edgeTypeInput.value;
 
+    console.log(edgeType);
     const nodeName = nodeNameInput.value;
     const selectedShape = selectedShapeInput ? selectedShapeInput.value : '';
 
-    console.log('Name:', nodeName);
-    console.log('Shape:', selectedShape);
-}
+    const parent = `${this.selectedNode}`;
+
+    this.onNodeClick(parent, selectedShape, nodeName, edgeType);
+  }
 }
